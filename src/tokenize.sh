9924244033ada5889a15b0f51228c791a083e411
MariaFjodorowa <laks.salmon@gmail.com>
2025-12-12 09:23:09 +0100

    upd args, time

--- src/tokenize.sh
+++ src/tokenize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=stat_dia
 #SBATCH --account=nn10029k
-#SBATCH --time=125:00:00
+#SBATCH --time=3:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
-#SBATCH --ntasks-per-node=122
+#SBATCH --ntasks-per-node=123
 #SBATCH --mem-per-cpu=1G
 #SBATCH --partition=normal
 
@@ -17,6 +17,6 @@ echo "SLURM_NTASKS: $SLURM_NTASKS"
 
 SIF="/cluster/projects/nn9851k/containers/pytorch2.7_cu2.9_py3.12_amd_nlpl.sif"
 
-srun apptainer exec -B /cluster/projects/:/cluster/projects/,/cluster/work/projects/:/cluster/work/projects/ $SIF python3 tokenize.py
+srun apptainer exec -B /cluster/projects/:/cluster/projects/,/cluster/work/projects/:/cluster/work/projects/ $SIF python3 tokenize.py ${@}
 
     
