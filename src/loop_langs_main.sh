25b119a5471e6691d7f57d5996ab870660b3e977
Andrey Kutuzov <andreku@ifi.uio.no>
2026-02-21 18:08:09 +0100

    Code for merging substitutions

--- src/loop_langs_main.sh
+++ src/loop_langs_main.sh
@@ -4,9 +4,10 @@ while read p;
 do
     echo ${p}
     # sbatch diachronic_mining.slurm ${p} 2024 2025 1000000
-    # sbatch filter_datasets.slurm ${p} /cluster/work/projects/nn9851k/mariiaf/diachronic/
+    # sbatch filter_datasets.slurm ${p} diachronic/
     # sbatch extract_text.slurm ${p} subsets
     # sbatch train_word2vec.slurm ${p} subsets
     # sbatch align_word2vec.slurm ${p} subsets
-    sbatch merge_embeddings.slurm ${p} /cluster/work/projects/nn9851k/mariiaf/diachronic/
+    # sbatch merge_embeddings.slurm ${p} diachronic/
+    sbatch merge_substitutions.slurm ${p} diachronic/
 done < ${1}
